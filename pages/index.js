af169a7cb4223eb90e712f37b5aa66c1200f1a5b
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-01-06 12:09:40 -0300

    Adding some regular components to use on the pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+// Data imports
+import { getFeaturedEvents } from '../dummy-data';
+
+// Components imports
+import EventList from '../components/events/event-list';
+
 const HomePage = () => {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>This is the Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
