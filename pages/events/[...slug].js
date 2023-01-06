21450cdf79588aa1b83d65e0772dbfb3c6d63954
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-01-06 17:39:45 -0300

    Working on the Filtered Events Page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,48 @@
+// Utils imports
+import { getFilteredEvents } from '../../dummy-data';
+import { useRouter } from 'next/router';
+
+// Components imports
+import EventList from '../../components/events/event-list';
+
 const FilteredEventsPage = () => {
+  const router = useRouter();
+
+  // if there is not parameters provided
+  if (!router.query.slug) {
+    return <p className='center'>Loading...</p>;
+  }
+
+  // extract query params
+  const year = +router.query.slug[0];
+  const month = +router.query.slug[1];
+
+  // handling invalid query params
+  if (
+    isNaN(year) ||
+    isNaN(month) ||
+    year > 2030 ||
+    year < 2021 ||
+    month < 1 ||
+    month > 12
+  ) {
+    return <p>Invalid date. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year,
+    month,
+  });
+
+  // Handling not found events
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found. Try a different date!</p>
+  }
+
+  // Happy path
   return (
     <div>
-      <h1>Filtered Events Page</h1>
+      <EventList items={filteredEvents} />
     </div>
   );
 };
