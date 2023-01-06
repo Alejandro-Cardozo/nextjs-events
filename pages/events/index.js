b90bc3dd36fa4293952eeb6b9b73768b6e71136a
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-01-06 17:11:43 -0300

    All events page and events search bar component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,28 @@
+// Utils imports
+import { useRouter } from 'next/router';
+
+// Data imports
+import { getAllEvents } from '../../dummy-data';
+
+// Components imports
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+
 const AllEventsPage = () => {
+  const events = getAllEvents();
+
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    router.push(`/events/${year}/${month}`)
+  }
+
   return (
-    <div>
-      <h1>All Events Page</h1>
-    </div>
-  )
-}
+    <>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </>
+  );
+};
 
 export default AllEventsPage;
