28d331c287c222e1823311dd699a6b25781a0bed
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-01-19 09:23:56 -0300

    Mongo DB connection and inserting newsletter

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,16 +1,21 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const email = req.body.email;
     if (!email || !email.includes('@')) {
       res.status(422).json({ message: 'Invalid email' });
       return;
     }
+
     try {
-      const date = new Date();
-      console.log(
-        `${email} has been successfully subscribed to our newsletter`
-      );
-      res.status(201).json({ email: email, time: date });
+      const client = await MongoClient.connect(process.env.MONGO_URI);
+      const db = client.db();
+      await db.collection('emails').insertOne({ email: email });
+      client.close();
+
+      const success = `${email} has been successfully subscribed to our newsletter`;
+      res.status(201).json({ message: success });
     } catch (error) {
       console.log(error);
     }
