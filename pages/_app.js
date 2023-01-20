5f380f89092475e0efe1aca478b948c9890cb286
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-01-20 09:57:26 -0300

    Showing and hidding notifications logic

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ import '../styles/globals.css';
 
 // Components imports
 import Layout from '../components/layout/layout';
-import Notification from '../components/ui/notification';
 import { NotificationContextProvider } from '../store/notification-context';
 
 export default function App({ Component, pageProps }) {
@@ -22,7 +21,6 @@ export default function App({ Component, pageProps }) {
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         </Head>
         <Component {...pageProps} />
-        <Notification title='Test' message='This is a test.' status='error' />
       </Layout>
     </NotificationContextProvider>
   );
--- store/notification-context.js
+++ store/notification-context.js
@@ -1,14 +1,30 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const NotificationContext = createContext({
   notification: null, // { title, message, status }
-  showNotification: () => {},
+  showNotification: (notificationData) => {},
   hideNotification: () => {},
 });
 
 export const NotificationContextProvider = (props) => {
+  const [activeNotification, setActiveNotification] = useState(null);
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hidNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hidNotificationHandler,
+  };
+
   return (
-    <NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
       {props.children}
     </NotificationContext.Provider>
   );
