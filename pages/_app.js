913ce8f21b52997d1c06f270f3eb23b9461129e6
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-01-20 09:07:40 -0300

    Building a component to show notifications

--- pages/_app.js
+++ pages/_app.js
@@ -6,16 +6,21 @@ import '../styles/globals.css';
 
 // Components imports
 import Layout from '../components/layout/layout';
+import Notification from '../components/ui/notification';
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
         <title>Next Events</title>
-        <meta name='description' content='Find the EVENT you will attend NEXT time' />
+        <meta
+          name='description'
+          content='Find the EVENT you will attend NEXT time'
+        />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />
+      <Notification title='Test' message='This is a test.' status='error' />
     </Layout>
   );
 }
