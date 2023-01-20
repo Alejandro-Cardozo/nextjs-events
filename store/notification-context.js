e2c86d4beec86e391ede061e3b2f0b35d4d8fd9a
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-01-20 10:37:37 -0300

    Hidding notifications on click or after 3 seconds

--- store/notification-context.js
+++ store/notification-context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const NotificationContext = createContext({
   notification: null, // { title, message, status }
@@ -8,6 +8,18 @@ const NotificationContext = createContext({
 
 export const NotificationContextProvider = (props) => {
   const [activeNotification, setActiveNotification] = useState(null);
+
+  useEffect(() => {
+    if (activeNotification && activeNotification.status !== 'pending') {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
 
   function showNotificationHandler(notificationData) {
     setActiveNotification(notificationData);
